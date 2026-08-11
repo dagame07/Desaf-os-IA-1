79037383800fae46ba942a8267f2e6e5ebecc295
dagame07 <denisse.garzas07@gmail.com>
2026-08-11 12:38:50 -0600

    Se agrega la funcion para filtrar arrays

---
+++ main.py
@@ -0,0 +1,6 @@
+
+lista = [1,1,1,2,3,4,5,5,5,5,5,8,8,9,0,11,15,16,17,18,19,20,20]
+revLista = list(set(lista))
+
+print ("Lista antes de revisión: ", lista)
+print ("Lista revisada y corregida: ", revLista)
